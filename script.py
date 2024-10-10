909246404599f8df36fa2a837964cccf5750afab
0x-arm <armenien0777@gmail.com>
2024-10-10 21:42:05 +0200

    Mise à jour du script

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
-print('Bonjour ! Ceci est un test !')
+class GitUser():
+	def __init__(self, name, link):
+		self.name = name
+		self.link = link
+
+	def about(self):
+		return f'Nom d\'utilisateur : {self.name}\nLien du profil : {self.link}'
+
+user = GitUser('0x-arm', 'https://github.com/0x-arm')
+
+print(user.about())
